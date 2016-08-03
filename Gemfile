c53648f0bfdc799f90fbeba6157553bec9988e38
Weissman <Jonathan.Weissman@wildlife.ca.gov>
2016-08-03 16:41:46 -0700

    added Gemfile for bundler

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'nokogiri'              # HTML parsing gem
+gem 'open-uri'              # http access
+gem 'fileutils'             # File manipulation
+gem 'csv'                   # CSV manipulation
+gem 'optparse'              # Cmd Line Input options
+gem 'optparse/date'
+gem 'geoutm'                # UTM/LLA conversions
+gem 'mechanize'             # web browsing
--- vecgdf.rb
+++ vecgdf.rb
@@ -1,32 +1,40 @@
 #!/usr/bin/env ruby
 
-# vectronic.rb
+# vecgdf.rb
 # This script downloads all collar data from the 
 # vectronic-wildlife website. 
 # It ignores collars with no data for the 
 # specified time period.
 
-require 'nokogiri'
-require 'mechanize'
-require 'open-uri'
-require 'fileutils'
-require 'csv'
-require 'optparse'
-require 'optparse/date'
-require 'geoutm'
 
+# Windows has special needs:
+require 'os' 
+if OS.windows? then require 'certified'
+
+require 'nokogiri'  		# HTML parsing gem
+require 'open-uri'  		# http access
+require 'fileutils'		# File manipulation
+require 'csv'			# CSV manipulation
+require 'optparse'		# Cmd Line Input options
+require 'optparse/date'			
+require 'geoutm'		# UTM/LLA conversions
+require 'mechanize'		# web browsing
+
+
+CSV_DELIM = "\t"
 
 # login stuff
 url = "https://www.vectronic-wildlife.com"
 
 
 # Command Line Options
-options = {:user => "cadfw1",
+options = {	   :user => "cadfw1",
 		   :db => "desertbighorn",
 		   :password => nil, 
 		   :dir => "./data/vectronic/",
 		   :beginDate => Date.today,
-		   :endDate => Date.today
+		   :endDate => Date.today,
+		   :verbose => nil
 }
 parser = OptionParser.new do |opts|
 	opts.banner = "Usage: vectronic.rb [options]"
@@ -54,28 +62,44 @@ parser = OptionParser.new do |opts|
 			'Ending date to retrieve data') do |endDate|
 		options[:endDate] = endDate
 	end
+	opts.on("-v", "--[no-]verbose", "Run verbosely") do |v| 
+		options[:verbose] = v
+		puts "Verbose output ON ..."
+	end
+
 end
 parser.parse!
 
+if options[:user] == nil
+	print "Enter user name: "
+	options[:user] = gets.chomp
+end
+
+if options[:password] == nil
+	print "Enter password: "
+	options[:password] = gets.chomp
+end
+
+
+
+# Set paramaters to user input: 
 userID = options[:user]
 dbName = options[:db]
 password = options[:password]
 
-
+# Build URL for http request to log in:
 loginParams = { 'type' => 'full',
 	'DB' => dbName, 
 	'UID' =>  userID, 
 	'PW' => password}
-urlLogin = "https://www.vectronic-wildlife.com/login.php?DB=" + 
+urlLogin = url + "/login.php?DB=" + 
 	dbName + "&PW=" + password + "&UID=" + userID + "&type=full"
+
 
 #Download info
 downloadDir = options[:dir].gsub(/([^\/]$)/, '\1/') # append / if none
 downloadDir += dbName + "/"
 FileUtils::mkdir_p downloadDir unless File.exists?(downloadDir)
-#class AttachmentSaver < Mechanize::File
-#	attr_reader :gdf
-CSV_DELIM = "\t"
 
 # Date Information
 dateStart = options[:beginDate]
@@ -93,26 +117,82 @@ a.pluggable_parser.csv = Mechanize::Download
 
 
 # Login:
+if options[:verbose] 
+	puts "Logging on to " + url
+	puts "Using username: " + userID + 
+		", database: " + dbName + 
+		", password: " + password.gsub(/./, '*') +
+		" ..."
+end
+
 a.post(urlLogin, loginParams)
 
+if options[:verbose]
+	if a.page.link_with(:text => /logout/i)
+		puts "Successfully logged in"
+	else
+		puts "There seems to be a login problem, check error log."
+	end
+end
+
 # Get list of collars
+
+if options[:verbose] 
+	puts "Generating list of available collars..."
+end
+
 collarList = Array::new
 a.page.parser.css("div#animalSelection option").each do |n|
 	collarList.push(n.text)
 end
 
+if options[:verbose] 
+	puts "Available collars: "
+	puts collarList
+end
+
+# Loop through collars, downloading data for each
+
+if options[:verbose] 
+	puts "Downloading collar data..."
+end
+
 collarList.each do |collar|
+
+	if options[:verbose] 
+		puts "Collar: " + collar
+	end
+	
 	dataUrl = url + "/getData.php?" + 
 				"animal=" + collar +
 				dateStr + 
 				"&type=table&tz=-420&maxCount=9999999"
 	a.get(dataUrl)
-
+	
+	linkToGdf = a.page.link_with(:text => /gdf/i)
+	
+	if options[:verbose] 
+		if linkToGdf.nil? 
+			puts "no data found"
+		else 
+			puts "Downloading data from " + url.gsub(/([^\/]$)/, '\1/') + linkToGdf.href
+		end
+	end
 	# Save GDF
-	if a.page.link_with(:text => /gdf/i) then 
-		r = a.page.link_with(:text => /gdf/i).click 
+	if linkToGdf then 
+		r = linkToGdf.click 
+		
+		if options[:verbose] 
+			puts "Saving data as " + downloadDir + r.filename + "..."
+		end
+		
 		r.save(downloadDir+r.filename)
 	end
-
+	
 end
 
+if options[:verbose] 
+	puts "Finished downloading data, exiting now."
+end
+
+end
