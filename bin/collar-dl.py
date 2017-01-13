31c6255ffd12d4593b04cea570a572d9865a27ed
Weissman <Jonathan.Weissman@wildlife.ca.gov>
2017-01-13 15:23:55 -0800

    gui for 3 downloaders

--- bin/collar-dl.py
+++ bin/collar-dl.py
@@ -7,6 +7,7 @@ Created on Mon Nov 21 16:24:18 2016
 
 from __future__ import print_function
 
+import time
 import argparse
 from gooey import Gooey, GooeyParser
 from subprocess import Popen, PIPE
@@ -14,6 +15,8 @@ from subprocess import Popen, PIPE
 running = True
 
 @Gooey(optional_cols=2, program_name="GPS Collar Data Download")
+
+
 
 def main():
   description_msg = 'Initiate batch downloads GPS collar data' \
@@ -31,29 +34,37 @@ def main():
   sirtrack_parser = subs.add_parser('sirtrack',help='https://data.sirtrack.com')
   
   #### Sirtrack     #####
-  # Usage: vectronic.rb [options]
-    # -u, --username user              Username
-    # -D, --database [database]        db name
-    # -p, --password password          Password
-    # -d, --dir dir                    Directory
-    # -h, --help                       Displays Help
-    # -b, --begin [date]               Beginning date to retrieve data
-    # -e, --end [date]                 Ending date to retrieve data
-    # -v, --[no-]verbose               Run verbosely
-    # -x, --csv [CSV_FILENAME]         Translate to CSV/TXT
-                                      # (using filename if supplied)
+#Usage: sirtrack.rb [options]
+#    -u, --username user              Username
+#    -p, --password password          Password
+#    -d, --dir dir                    Directory
+#    -h, --help                       Displays Help
+#    -v, --[no-]verbose               Run verbosely
+#    -x, --csv [CSV_FILENAME]         Translate to single CSV/TXT
+#                                      (using filename if supplied)
+#    -z, --debug                      Run in debug mode
+
 
   sirtrack_parser.add_argument('-u', '--username', type=str,
-                               help='User name for your account')
+                               help='User name for your account', 
+                               default='david.german@wildlife.ca.gov')
   sirtrack_parser.add_argument('-p', '--password', type=str,
                                help='Password for your account (not secure)')
   sirtrack_parser.add_argument('-d', '--dir', type=str,
-                               help='Directory in which to download data files')
-#  sirtrack_parser.add_argument('-h', '--help', dest='help', action="store_true",
-#                               help='Display help with command options')
-  #sirtrack_parser.add_argument('-v', '--verbose', dest='verbose)        
-  sirtrack_parser.add_argument('-x', '--csv', type=str, 
-                               help='Translate to a single CSV/TXT file')                            
+                               help='Directory in which to download data files',
+                               default='data')
+#  sirtrack_parser.add_argument('-b', '--begin', type=str, widget='DateChooser',
+#                                help='beginning date to retrieve data from')          
+#  sirtrack_parser.add_argument('-e', '--end', type=str, widget='DateChooser',
+#                                help='ending date to retrieve data from')                            
+  # sirtrack_parser.add_argument('-h', '--help', dest='help', action="store_true",
+  #                               help='Display help with command options')
+  sirtrack_filename = 'sirtrack' + time.strftime("%Y%m%d") + '.txt' 
+#  sirtrack_parser.add_argument('-x', '--csv', type=str, 
+#                               help='Translate to a single CSV/TXT file',
+#                               default=sirtrack_filename)
+  sirtrack_parser.add_argument('-v', '--verbose', action='store_true',
+                                help='output details of downloading process') 
                             
   #### Vectronic   #### 
   # Usage: vectronic.rb [options]
@@ -66,8 +77,7 @@ def main():
     # -e, --end [date]                 Ending date to retrieve data
     # -v, --[no-]verbose               Run verbosely
     # -x, --csv [CSV_FILENAME]         Translate to CSV/TXT
-
-    
+                            
   vectronic_parser.add_argument('-u', '--username', type=str,
                                help='User name for your account', default='snbs')
   vectronic_parser.add_argument('-p', '--password', type=str,
@@ -80,17 +90,47 @@ def main():
                                 help='beginning date to retrieve data from')          
   vectronic_parser.add_argument('-e', '--end', type=str, widget='DateChooser',
                                 help='ending date to retrieve data from')                                       
-#  vectronic_parser.add_argument('-h', '--help', dest='help', action="store_true",
-#                               help='Display help with command options')
-  #vectronic_parser.add_argument('-v', '--verbose', dest='verbose)        
+  vectronic_filename = 'vectronic_' + time.strftime("%Y%m%d") + '.txt'                            
   vectronic_parser.add_argument('-x', '--csv', type=str, 
-                               help='Translate to a single CSV/TXT file')                            
-                              
+                               help='Translate to a single CSV/TXT file. Enter name of file.',
+                               default=vectronic_filename)                            
+  vectronic_parser.add_argument('-v', '--verbose', action='store_true',
+                                help='output details of downloading process')                             
                                
   
   #### Vectronic    #####
   
   #### Tellus       #####
+#  Usage: tellus.rb [options]
+#    -u, --username user              Username
+#    -p, --password password          Password
+#    -d, --dir dir                    Directory
+#    -h, --help                       Displays Help
+#    -b, --begin [date]               Beginning date to retrieve data
+#    -e, --end [date]                 Ending date to retrieve data
+#    -v, --[no-]verbose               Run verbosely
+#    -x, --csv [CSV_FILENAME]         Translate to single CSV/TXT
+#                                      (using filename if supplied)
+#    -z, --debug                      Run in debug mode
+  tellus_parser.add_argument('-u', '--username', type=str,
+                               help='User name for your account', default='david@desert')
+  tellus_parser.add_argument('-p', '--password', type=str,
+                               help='Password for your account (not secure)')
+  tellus_parser.add_argument('-d', '--dir', type=str, widget="DirChooser",
+                               help='Directory in which to download data files', default='data')
+  tellus_parser.add_argument('-b', '--begin', type=str, widget='DateChooser',
+                                help='beginning date to retrieve data from')          
+  tellus_parser.add_argument('-e', '--end', type=str, widget='DateChooser',
+                                help='ending date to retrieve data from')                                       
+  tellus_filename = 'tellus_' + time.strftime("%Y%m%d") + '.txt'                            
+  tellus_parser.add_argument('-x', '--csv', type=str, 
+                               help='Translate to a single CSV/TXT file. Enter name of file.',
+                               default=tellus_filename)                            
+  tellus_parser.add_argument('-v', '--verbose', action='store_true',
+                                help='output details of downloading process')        
+
+
+  #### Convert arguments to command line string: ####
   interpreter = 'ruby'
   args = parser.parse_args()
   dargs = args.__dict__  
@@ -104,17 +144,20 @@ def main():
       elif len(key) > 1 :
         prefix = "--"
       option = prefix + key
-      arg = dargs[key]
       arg_list.append(option)
-      arg_list.append(arg)
+      arg = dargs[key]
+      if type(arg) == str :
+        arg_list.append(arg)
           
   collardl_command = [interpreter, program_file] + arg_list
   # program_file = args.command + '.rb'
   print( "running the following command:")
   print(args)
   print( ' '.join(collardl_command))
-  p = Popen(collardl_command)
-  print(p)
+  dl_cmd = Popen(collardl_command)
+  # for line in dl_cmd.stdout.readlines():
+    # print(line)
+  # dl_cmd.wait()
   print()
   #print(args)
   #print( os.system(cmd) )
