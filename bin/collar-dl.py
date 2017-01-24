84922652cfa11bf494647f3dd6cc5741b55f7714
Weissman <Jonathan.Weissman@wildlife.ca.gov>
2017-01-24 14:40:06 -0800

    working... sort of

--- bin/collar-dl.py
+++ bin/collar-dl.py
@@ -22,45 +22,45 @@ running = True
 # nonbuffered_stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 # sys.stdout = nonbuffered_stdout
 
-def subprocess_args(include_stdout=True):
-    # The following is true only on Windows.
-    if hasattr(subprocess, 'STARTUPINFO'):
-        # On Windows, subprocess calls will pop up a command window by default
-        # when run from Pyinstaller with the ``--noconsole`` option. Avoid this
-        # distraction.
-        si = subprocess.STARTUPINFO()
-        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-        # Windows doesn't search the path by default. Pass it an environment so
-        # it will.
-        env = os.environ
-    else:
-        si = None
-        env = None
+# def subprocess_args(include_stdout=True):
+    # # The following is true only on Windows.
+    # if hasattr(subprocess, 'STARTUPINFO'):
+        # # On Windows, subprocess calls will pop up a command window by default
+        # # when run from Pyinstaller with the ``--noconsole`` option. Avoid this
+        # # distraction.
+        # si = subprocess.STARTUPINFO()
+        # si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        # # Windows doesn't search the path by default. Pass it an environment so
+        # # it will.
+        # env = os.environ
+    # else:
+        # si = None
+        # env = None
 
-    # ``subprocess.check_output`` doesn't allow specifying ``stdout``::
-    #
-    #   Traceback (most recent call last):
-    #     File "test_subprocess.py", line 58, in <module>
-    #       **subprocess_args(stdout=None))
-    #     File "C:\Python27\lib\subprocess.py", line 567, in check_output
-    #       raise ValueError('stdout argument not allowed, it will be overridden.')
-    #   ValueError: stdout argument not allowed, it will be overridden.
-    #
-    # So, add it only if it's needed.
-    if include_stdout:
-        ret = {'stdout:': subprocess.PIPE}
-    else:
-        ret = {}
+    # # ``subprocess.check_output`` doesn't allow specifying ``stdout``::
+    # #
+    # #   Traceback (most recent call last):
+    # #     File "test_subprocess.py", line 58, in <module>
+    # #       **subprocess_args(stdout=None))
+    # #     File "C:\Python27\lib\subprocess.py", line 567, in check_output
+    # #       raise ValueError('stdout argument not allowed, it will be overridden.')
+    # #   ValueError: stdout argument not allowed, it will be overridden.
+    # #
+    # # So, add it only if it's needed.
+    # if include_stdout:
+        # ret = {'stdout:': subprocess.PIPE}
+    # else:
+        # ret = {}
 
-    # On Windows, running this from the binary produced by Pyinstaller
-    # with the ``--noconsole`` option requires redirecting everything
-    # (stdin, stdout, stderr) to avoid an OSError exception
-    # "[Error 6] the handle is invalid."
-    ret.update({'stdin': subprocess.PIPE,
-                'stderr': subprocess.PIPE,
-                'startupinfo': si,
-                'env': env })
-    return ret
+    # # On Windows, running this from the binary produced by Pyinstaller
+    # # with the ``--noconsole`` option requires redirecting everything
+    # # (stdin, stdout, stderr) to avoid an OSError exception
+    # # "[Error 6] the handle is invalid."
+    # ret.update({'stdin': subprocess.PIPE,
+                # 'stderr': subprocess.PIPE,
+                # 'startupinfo': si,
+                # 'env': env })
+    # return ret
 
 
 def main():
@@ -200,19 +200,19 @@ def main():
   print(args)
   print( ' '.join(collardl_command))
   
-  #dl_cmd = Popen(collardl_command)
+  dl_cmd = Popen(collardl_command)
   
-  try: 
-    output = subprocess.check_output(collardl_command,
-                                     **subprocess_args(True)))
-    print(output)
-  except OSError as e:
-    print('Failed: ' + str(e) ) 
+  # try: 
+    # output = subprocess.check_output(collardl_command,
+                                     # **subprocess_args(True)))
+    # print(output)
+  # except OSError as e:
+    # print('Failed: ' + str(e) ) 
   
   # for line in dl_cmd.stdout.readlines():
     # print(line)
   # dl_cmd.wait()
-  print()
+  # print()
   #print(args)
   #print( os.system(cmd) )
   
